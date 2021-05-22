7fc2c7c43d09d1f587d91bf6911dca5f65f6c601
amv1017 <amv1017@mail.ru>
2021-05-22 20:15:05 +0300

    workable

--- models/painting.go
+++ models/painting.go
@@ -4,7 +4,5 @@ type Painting struct {
 	ID			uint	`json:"id" gorm:"primary_key"`
 	Title		string	`json:"title" gorm:"primary_key"`
 	Description	string	`json:"description"`
-
-	Genre		Genre	`json:"genre" gorm:"foreignKey:Title"`
 	Url			string	`json:"url"`
 }
