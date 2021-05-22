b9ef5fdbb7e864169fd1786621c1dc28238c951a
amv1017 <amv1017@mail.ru>
2021-05-22 20:07:11 +0300

    check constraint

--- models/genre.go
+++ models/genre.go
@@ -1,7 +1,7 @@
 package models
 
 type Genre struct {
-	ID			uint		`json:"id" gorm:"primary_key"`
-	Title		string		`json:"name" gorm:"primary_key"`
+	ID		uint	`json:"id" gorm:"primary_key"`
+	Title	string	`json:"name" gorm:"primary_key"`
 }
 
--- models/painting.go
+++ models/painting.go
@@ -4,5 +4,7 @@ type Painting struct {
 	ID			uint	`json:"id" gorm:"primary_key"`
 	Title		string	`json:"title" gorm:"primary_key"`
 	Description	string	`json:"description"`
+
+	Genre		Genre	`json:"genre" gorm:"foreignKey:Title"`
 	Url			string	`json:"url"`
 }
